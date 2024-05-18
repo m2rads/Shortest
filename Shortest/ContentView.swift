5030e4a8c816b10892b7c8dba6894097000cce43
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-17 17:23:05 -0700

    Initial Commit

---
+++ Shortest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shortest
+//
+//  Created by m2rads on 2024-05-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shortest/ShortestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortestApp.swift
+//  Shortest
+//
+//  Created by m2rads on 2024-05-17.
+//
+
+import SwiftUI
+
+@main
+struct ShortestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
