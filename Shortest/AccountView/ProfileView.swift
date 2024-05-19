2f9cb0bbe37485e66c95cae10ffa624939c7f5a0
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-19 00:10:46 -0700

    add initial profile loader

--- Shortest/AccountView/ProfileView.swift
+++ Shortest/AccountView/ProfileView.swift
@@ -10,35 +10,73 @@ import Supabase
 
 struct ProfileView: View {
     @State var username = ""
-    @State var fullname = ""
+    @State var fullName = ""
     @State var website = ""
     
     @State var isLoading = false
     
     var body: some View {
-        Section {
-            TextField("Username", text: $username)
+        NavigationStack {
+          Form {
+            Section {
+              TextField("Username", text: $username)
                 .textContentType(.username)
                 .textInputAutocapitalization(.never)
-            TextField("Full Name", text: $fullname)
+              TextField("Full name", text: $fullName)
                 .textContentType(.name)
-            TextField("Website", text: $website)
+              TextField("Website", text: $website)
                 .textContentType(.URL)
                 .textInputAutocapitalization(.never)
-        }
-        
-        Section {
-            Button("Update profile") {
+            }
+
+            Section {
+              Button("Update profile") {
                 updateProfileButtonTapped()
-            }
-            .bold()
-            
-            if isLoading {
+              }
+              .bold()
+
+              if isLoading {
                 ProgressView()
+              }
             }
+          }
+          .navigationTitle("Profile")
+          .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading){
+              Button("Sign out", role: .destructive) {
+                Task {
+                  try? await supabase.auth.signOut()
+                }
+              }
+            }
+          })
+        }
+        .task {
+          await getInitialProfile()
+        }
+      }
+    
+    func getInitialProfile() async {
+        do {
+            let currentUser = try await supabase.auth.session.user
+
+            let profile: Profile = try await supabase
+                .from("profiles")
+                .select()
+                .eq("id", value: currentUser.id)
+                .single()
+                .execute()
+                .value
+            
+            self.username = profile.username ?? ""
+            self.fullName = profile.fullName ?? ""
+            self.website = profile.website ?? ""
+            
+        } catch {
+            debugPrint(error)
         }
     }
-    
+
     func updateProfileButtonTapped() {
         Task {
             isLoading = true
@@ -51,7 +89,7 @@ struct ProfileView: View {
                     .update(
                         UpdateProfileParams(
                             username: username,
-                            fullName: fullname,
+                            fullName: fullName,
                             website: website
                         )
                     )
