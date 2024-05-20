51d5a43d99fc651084e59972fe5e326979866005
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-20 13:01:30 -0700

    add auth manager and apple sing in manager

---
+++ Shortest/Helper/AuthManager.swift
@@ -0,0 +1,21 @@
+//
+//  AuthManager.swift
+//  Shortest
+//
+//  Created by m2rads on 2024-05-20.
+//
+
+import Foundation
+
+
+class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    func signInWithApple(idToken: String, nonce: String) async throws {
+        let sessison = try await supabase.auth.signInWithIdToken(credentials: .init(provider: .apple, idToken: idToken, nonce: nonce))
+    }
+    
+}
