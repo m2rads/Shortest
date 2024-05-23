9a3dd9df4a00937b3e125c5389c0c9998eb5e534
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-22 17:50:54 -0700

    add navigation to edit profile view

--- Shortest/Views/AccountView/EditProfile.swift
+++ Shortest/Views/AccountView/EditProfile.swift
@@ -8,58 +8,52 @@
 import SwiftUI
 import Supabase
 
-struct ProfileView2: View {
+struct EditProfile: View {
     @State var username = ""
     @State var fullName = ""
     @State var website = ""
-    
     @State var isLoading = false
+    
+    @Binding var appUser: AppUser?
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         NavigationStack {
-          Form {
-            Section {
-              TextField("Username", text: $username)
-                .textContentType(.username)
-                .textInputAutocapitalization(.never)
-              TextField("Full name", text: $fullName)
-                .textContentType(.name)
-              TextField("Website", text: $website)
-                .textContentType(.URL)
-                .textInputAutocapitalization(.never)
-            }
-
-            Section {
-              Button("Update profile") {
-                updateProfileButtonTapped()
-              }
-              .bold()
-
-              if isLoading {
-                ProgressView()
-              }
-            }
-          }
-          .navigationTitle("Profile")
-          .toolbar(content: {
-            ToolbarItem(placement: .topBarLeading){
-              Button("Sign out", role: .destructive) {
-                Task {
-                  try? await supabase.auth.signOut()
+            Form {
+                Section {
+                    TextField("Username", text: $username)
+                        .textContentType(.username)
+                        .textInputAutocapitalization(.never)
+                    TextField("Full name", text: $fullName)
+                        .textContentType(.name)
+                    TextField("Website", text: $website)
+                        .textContentType(.URL)
+                        .textInputAutocapitalization(.never)
                 }
-              }
+                
+                Section {
+                    Button("Update profile") {
+                        updateProfileButtonTapped()
+                    }
+                    .bold()
+                    
+                    if isLoading {
+                        ProgressView()
+                    }
+                }
             }
-          })
+            .navigationTitle("Edit Profile")
         }
         .task {
-          await getInitialProfile()
+            await getInitialProfile()
         }
-      }
+    }
+    
     
     func getInitialProfile() async {
         do {
             let currentUser = try await supabase.auth.session.user
-
+            
             let profile: Profile = try await supabase
                 .from("profiles")
                 .select()
@@ -71,12 +65,12 @@ struct ProfileView2: View {
             self.username = profile.username ?? ""
             self.fullName = profile.fullName ?? ""
             self.website = profile.website ?? ""
-                        
+            
         } catch {
             debugPrint(error)
         }
     }
-
+    
     func updateProfileButtonTapped() {
         Task {
             isLoading = true
@@ -103,5 +97,5 @@ struct ProfileView2: View {
 }
 
 #Preview {
-    ProfileView2()
+    EditProfile(appUser: .constant(.init(uid: "12345", email: "hello@example.com")))
 }
--- Shortest/Views/AccountView/ProfileView.swift
+++ Shortest/Views/AccountView/ProfileView.swift
@@ -12,24 +12,89 @@ struct ProfileView: View {
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        ScrollView {
-            Spacer()
-            VStack(alignment: .leading) {
-                // Top buttons for navigation and editing
-                Spacer()
-                HStack {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    // Profile picture and info
+                    HStack(alignment: .top) {
+                        Image("ppfPlaceHolder")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                            .padding(.leading)
+                        
+                        VStack(alignment: .leading) {
+                            Text("@Mohrad23")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            
+                            Text("Mohammad Rad")
+                                .font(.title2)
+                                .bold()
+                        }
+                        .padding(.leading)
+                        Spacer()
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 20)
+                    
+                    // Bio and website
+                    VStack(alignment: .leading) {
+                        Text("just a dude who codes · my notebook 👉 [github.com/m2rads](https://github.com/m2rads)")
+                            .font(.body)
+                            .padding(.bottom, 5)
+                        
+                        HStack {
+                            HStack {
+                                Text("4")
+                                    .bold()
+                                Text("Following")
+                                Text("1")
+                                    .bold()
+                                Text("Followers")
+                            }
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                            .padding(.top, 5)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                print("clicked")
+                            }, label: {
+                                Image(systemName: "gift.circle.fill")
+                                    .foregroundColor(.gray)
+                            })
+                            .padding(.bottom, -6)
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    // Empty state for messages
+                    VStack {
+                        Text("No thoughts yet")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                            .padding()
+                        Spacer()
+                    }
+                }
+                .padding(.top, 20)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: {
                         // Action for going back
                     }) {
                         Image(systemName: "chevron.left")
                             .foregroundColor(.gray)
-                            .padding()
                     }
-                    Spacer()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Menu {
-                        Button(action: {
-                            // Action for editing profile
-                        }) {
+                        NavigationLink(destination: EditProfile(appUser: $appUser)) {
                             Text("Edit Profile")
                         }
                         Button(action: {
@@ -47,80 +112,10 @@ struct ProfileView: View {
                     } label: {
                         Image(systemName: "ellipsis")
                             .foregroundColor(.gray)
-                            .padding()
                     }
-                }
-                .padding(.top, 10)
-                
-                // Profile picture and info
-                HStack(alignment: .top) {
-                    Image("profilePicture")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
-                        .padding(.leading)
-                    
-                    VStack(alignment: .leading) {
-                        Text("@Mohrad23")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        
-                        Text("Mohammad Rad")
-                            .font(.title2)
-                            .bold()
-                    }
-                    .padding(.leading)
-                    Spacer()
-                }
-                .padding(.top, 20)
-                .padding(.bottom, 20)
-                
-                // Bio and website
-                VStack(alignment: .leading) {
-                    Text("just a dude who codes · my notebook 👉 [github.com/m2rads](https://github.com/m2rads)")
-                        .font(.body)
-                        .padding(.bottom, 5)
-                    
-                    HStack {
-                        HStack {
-                            Text("4")
-                                .bold()
-                            Text("Following")
-                            Text("1")
-                                .bold()
-                            Text("Followers")
-                        }
-                        .font(.footnote)
-                        .foregroundColor(.gray)
-                        .padding(.top, 5)
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            print("clicked")
-                        }, label: {
-                            Image(systemName: "gift.circle.fill")
-                                .foregroundColor(.gray)
-                        })
-                        .padding(.bottom, -6)
-                    }
-                }
-                .padding(.horizontal)
-                
-                // Empty state for messages
-                VStack {
-                    Text("No thoughts yet")
-                        .font(.body)
-                        .foregroundColor(.gray)
-                        .padding()
-                    Spacer()
                 }
             }
-            .padding(.top, 20)
         }
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
