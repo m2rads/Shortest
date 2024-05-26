0284211fd16a25daa22139517c8e1799b4857df3
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-26 11:34:58 -0700

    refactor update params

--- Shortest/Models/DataModel.swift
+++ Shortest/Models/DataModel.swift
@@ -7,26 +7,12 @@
 
 import Foundation
 
-struct Profile: Decodable {
-  let username: String?
-  let fullName: String?
-  let website: String?
-
-  enum CodingKeys: String, CodingKey {
-    case username
-    case fullName = "full_name"
-    case website
-  }
-}
-
 struct UpdateProfileParams: Encodable {
   let username: String
-  let fullName: String
-  let website: String
+  let bio: String
 
   enum CodingKeys: String, CodingKey {
     case username
-    case fullName = "full_name"
-    case website
+    case bio
   }
 }
