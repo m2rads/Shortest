964902119611c018b4bf13de608128ab4d7f0b86
Mohammad Rad <argo.mohrad@gmail.com>
2024-06-15 19:30:10 -0700

    update group view model

--- Shortest/Views/Group/GroupView.swift
+++ Shortest/Views/Group/GroupView.swift
@@ -11,28 +11,45 @@ struct GroupView: View {
     @State private var showJoinConfirmation = false
     @State var group: GroupModel
     @State var isMember = false
+    @State var isOwner = false
+    @State private var messageText = ""
+    @State private var messages: [Message] = []
     
     var body: some View {
         VStack {
-            Text(group.name)
-                .font(.title)
-                .bold()
-                .padding()
-            
-            Text(group.description ?? "")
-                .font(.body)
-                .padding([.leading, .trailing])
-            
-            Spacer()
-            
-            if isMember {
-                Button(action: {
-                    // Action to add a new post
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                        .font(.largeTitle)
-                        .foregroundColor(.blue)
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(messages, id: \.id) { message in
+                        HStack {
+                            Text(message.content)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        .padding(.horizontal)
+                        .padding(.top, 4)
+                    }
                 }
+            }
+            
+            if isMember || isOwner {
+                HStack {
+                    TextField("Enter your message...", text: $messageText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(minHeight: 30)
+                    
+                    Button(action: {
+                        Task {
+                            await sendMessage()
+                        }
+                    }) {
+                        Image(systemName: "paperplane.fill")
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                }
+                .padding()
             } else {
                 Button(action: {
                     showJoinConfirmation.toggle()
@@ -46,6 +63,7 @@ struct GroupView: View {
                                 .foregroundColor(Color(uiColor: .label))
                         }
                 }
+                .padding()
                 .alert(isPresented: $showJoinConfirmation) {
                     Alert(
                         title: Text("Join Group"),
@@ -60,18 +78,71 @@ struct GroupView: View {
                 }
             }
         }
-        .padding()
         .navigationTitle(group.name)
         .task {
             await checkMembership()
+            await checkOwnership()
+            await loadMessages()
+        }
+    }
+    
+    private func sendMessage() async {
+        // Logic to send the message
+        guard !messageText.isEmpty else { return }
+        
+        do {
+            let userId = try await supabase.auth.session.user.id
+            
+            let newMessage = Message(
+                id: UUID(),
+                content: messageText,
+                group_id: group.id,
+                user_id: userId,
+                timestamp: Date()
+            )
+            
+            messages.append(newMessage)
+            messageText = ""
+            saveMessage(newMessage)
+        } catch {
+            
+        }
+    }
+    
+    private func saveMessage(_ message: Message) {
+        Task {
+            do {
+                try await supabase
+                    .from("messages")
+                    .insert(message)
+                    .single()
+                    .execute()
+            } catch {
+                print("Error saving message: \(error)")
+            }
+        }
+    }
+    
+    private func loadMessages() async {
+        do {
+            let fetchedMessages: [Message] = try await supabase
+                .from("messages")
+                .select()
+                .eq("group_id", value: group.id)
+                .execute()
+                .value
+            
+            messages = fetchedMessages
+        } catch {
+            print("Error loading messages: \(error)")
         }
     }
     
     private func joinGroup() async {
         do {
             let userId = try await supabase.auth.session.user.id
-            let groupMembership = GroupMembership(id: UUID(), groupId: group.id ?? UUID(), userId: userId, joinedAt: Date())
-            let membership: GroupMembership = try await supabase
+            let groupMembership = GroupMembership(id: UUID(), group_id: group.id, user_id: userId, joined_at: Date())
+            try await supabase
                 .from("group_memberships")
                 .insert(groupMembership)
                 .single()
@@ -100,17 +171,19 @@ struct GroupView: View {
             print("Error checking membership: \(error)")
         }
     }
-}
-
-struct GroupMembership: Codable {
-    let id: UUID
-    let groupId: UUID
-    let userId: UUID
-    let joinedAt: Date
+    
+    private func checkOwnership() async {
+        do {
+            let userId = try await supabase.auth.session.user.id
+            isOwner = group.creator_id == userId
+        } catch {
+            print("Error checking ownership: \(error)")
+        }
+    }
 }
 
 
 #Preview {
-    GroupView(group: .init(id: UUID(), name: "Test Group", description: "Testing the first Group", creatorId: UUID()))
+    GroupView(group: .init(id: UUID(), name: "Test Group", description: "Testing the first Group", creator_id: UUID()))
 }
 
